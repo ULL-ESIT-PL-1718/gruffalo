e4a472ba33aad7aa957100d418e196c4deb1d2eb
Casiano Rodriguez Leon <casiano.rodriguez.leon@gmail.com>
2017-12-25 15:05:38 +0000

    bin/whitespaces-parenthesis.js

--- bin/whitespaces-parenthesis.js
+++ bin/whitespaces-parenthesis.js
@@ -1,34 +1,35 @@
 
-const fs = require('fs')
+const fs = require('fs');
 
-let input = '(((1)))'
+let input = '  (  (  (  1)  )    )';
 
-const gruffalo = require('../gruffalo')
+const gruffalo = require('../gruffalo');
 
-let grammar = new gruffalo.Grammar({ start: 'E' })
+let grammar = new gruffalo.Grammar({ start: 'E' });
 let rule = (left, right, f) => {
   return grammar.add(new gruffalo.Rule(left, right, f));
 };
 
-rule('E', ['LP', 'E', 'RP'], (LP, E, RP) => { return [ E ] });
+rule('E', ['LP', 'E', 'RP'], (LP, E, RP) => [ E ]);
 rule('E', ['ONE'], (one) => one);
 
-rule('ONE', ['1'], (a) => a.type);
+rule('ONE', ['_', '1', '_'], (_, a) => a.type);
 rule ('LP', ['_', '(', '_'], () => '');
 rule ('RP', ['_', ')'], () => '');
-//rule ('RP', ['_', ')', '_'], () => '');
+// Substituting the former rule by this one produces an error. Seems to be a bug?
+//rule ('RP', ['_', ')', '_'], () => ''); 
 
 // whites
 rule('_', []);
 rule('_', ['_', ' ']); 
 
 
-let p = eval(gruffalo.compile(grammar))({})
+let p = eval(gruffalo.compile(grammar))({});
 
-var index = 0
+var index = 0;
 function lex() {
-  return { type: input[index++] || '$' }
+  return { type: input[index++] || '$' };
 }
 
-console.log(JSON.stringify(p(lex)))
+console.log(JSON.stringify(p(lex)));
 
